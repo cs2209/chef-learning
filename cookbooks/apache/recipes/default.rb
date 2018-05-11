be01eb7b57c74f83fa4efadfb4d31ca3595149f9
cs2209 <chetansharma.cs6@gmail.com>
2018-05-11 12:15:25 +0530

    added motd and version

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,13 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+if node['platform_family'] == "debian"
+package = "apache2"
+elseif node['platform_family'] == "rhel"
+package = "httpd"
+end 
+
 package "apache2" do
- package_name "apache2" 
+ package_name package 
   action :install
 end 
 
 service "apache2" do
 service_name "apache2"
-action :enable
+action [:start,:enable]
 end 
 
